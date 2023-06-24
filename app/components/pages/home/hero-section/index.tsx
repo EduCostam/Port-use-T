1debdf06d3a995f8c6d9e3d74f6e58dafa969a2f
EduCostam <eduardomc.devs@gmail.com>
2023-06-24 16:32:02 -0300

    ajustes de div

--- app/components/pages/home/hero-section/index.tsx
+++ app/components/pages/home/hero-section/index.tsx
@@ -1,28 +1,27 @@
+"use client";
+
 import Image from "next/image";
 
 export const HeroSection = () => {
   return (
     <section className="w-full h-[755px] bg-hero-image bg-cover bg-center bg-no-repeat flex flex-col justify-end pb-10 sm:pb-32 py-32 lg:pb-[110px]">
-      <div className="container flex items-start justify-between flex-col-reverse lg:flex-grow">
-        <div>
+      <div className="container flex items-start justify-between flex-col-reverse lg:flex-row">
+        <div className="w-full lg:max-w-[530px]">
           <p>Olá, meu nome é</p>
           <h2>Eduardo Marques</h2>
 
-          <div>
-            <p>Olá, meu nome é Eduardo, desenvolvedor web</p>
+          <p>Olá, meu nome é Eduardo, desenvolvedor web</p>
 
-            <div>techs</div>
+          <div>techs</div>
 
-            <div>contato</div>
-          </div>
-
-          <Image
-            width={420}
-            height={404}
-            src="/images/profile-pic.png"
-            alt="Foto de perfil do Eduardo Marques"
-          />
+          <div>contato</div>
         </div>
+        <Image
+          width={420}
+          height={404}
+          src={"/images/profile-pic.png"}
+          alt="Foto de perfil do Eduardo Marques"
+        />
       </div>
     </section>
   );
